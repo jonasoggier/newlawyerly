256e2391e8e075ed6ffbeb14d53562e3cd8ec9e9
Jonas Oggier <jonas.oggier@gmail.com>
2013-07-11 18:10:23 +0200

    Styling homepage image area

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,9 @@
 class HomeController < ApplicationController
 
-	skip_before_filter :require_login  # TBD if sufficient from security perspective!
+	skip_before_filter :require_login  
 
 	def index
-		if logged_in?
       @feed_items = current_user.feed.page params[:page] 
-  		@users = User.all  # this is for the temporary list of users on homepage
-  	end
 	end
 	
 end
