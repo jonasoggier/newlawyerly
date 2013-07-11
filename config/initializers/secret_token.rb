37b878691a55549844fb98d4d4707d91a0bda9c7
Jonas Oggier <jonas.oggier@gmail.com>
2013-07-11 10:17:55 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Newlawyerly::Application.config.secret_key_base = 'ea82601115b42b2111d8b12f52a5de87a40c6639e2c241fc285246a6decbbae1a3d023a045720beabcff355a9dd8a1e6e660de298df596023e7d22649b1eb9da'
